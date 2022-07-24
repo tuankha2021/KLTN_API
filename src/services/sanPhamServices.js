923c07c0e88d5a53d677047231ee58d53f550186
tuankha <18520873@gm.uit.edu.vn>
2022-07-24 21:37:15 +0700

    update

--- src/services/sanPhamServices.js
+++ src/services/sanPhamServices.js
@@ -3,16 +3,37 @@ import db from "../models/index";
 const getAllSP = async () => {
     try {
 
-        let user = await db.SanPham.findAll({
-            include: { model: db.LoaiSanPham },
-        });
+        let data = [];
 
-        if (user) {
+        let loaisp = await db.LoaiSanPham.findAll({
+            attributes: ['id', 'TenLoai', 'MinDate', 'HSD'],
+        })
+
+        for (let i in loaisp) {
+
+            let obj = {
+                AddData: function (loaisanpham, sanpham) {
+                    this.loaisanpham = loaisanpham;
+                    this.sanpham = sanpham;
+                }
+            };
+
+            let user = await db.SanPham.findAll({
+                where: { LoaiSanPhamId: loaisp[i].id },
+            });
+
+            obj.sanpham = user;
+            obj.loaisanpham = loaisp[i];
+            data.push(obj);
+
+        }
+
+        if (data) {
             // let data = user.get({ plain: true })
             return {
                 EM: "get data success",
                 EC: 0,
-                DT: user
+                DT: data
             }
         } else {
             return {
